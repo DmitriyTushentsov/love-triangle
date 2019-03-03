7f0333ca204488f643a07054a4b6ad64c3881cf5
Dmitriy <fr80@list.ru>
2019-03-03 18:40:33 +0300

    feat: add function for calculate love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    preferences.unshift(0);
+    let summ = 0;
+    let obj = {};
+    for (let i = 1; i < preferences.length; i++) {
+        let first = preferences[i];
+        let second = preferences[first];
+        let third = preferences[second];
+        if (third === i && obj[i] === undefined && obj[first] === undefined && obj[second] === undefined && first !== second && first !== third && second !== third) {
+            obj[i] = true;
+            obj[[preferences[i]]] = true;
+            obj[[preferences[preferences[i]]]] = true;
+            summ++;
+        }
+    }
+    return summ;
 };
